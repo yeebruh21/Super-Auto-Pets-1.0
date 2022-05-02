2829bfae5f767eee57266f91a7916e99d4cb4b19
zshuhanzz <99359165+zshuhanzz@users.noreply.github.com>
2022-05-01 23:57:17 -0400

    Added documentation
    Added java docs documentation for background class

--- Background.java
+++ Background.java
@@ -6,13 +6,18 @@ import java.util.*;
  */
 public class Background {
 
+	/** The name of the current background equipped*/
 	private String currentBackground;
+	/** Hashmap of the shops and prices*/
 	private HashMap<String, Integer> backgrounds=new HashMap<String, Integer>();
+	/** Arraylist of the owned maps*/
 	private ArrayList<String> owned = new ArrayList<String>();
 
 
+	/**
+	 * The constructor for the background class. Initializes the available backgrounds in the game.
+	 */
 	public Background(){
-
 		backgrounds.put("Grass Field", 0);
 		backgrounds.put("Hot Desert", 5000);
 		backgrounds.put("Lava Mountain", 5000);
@@ -21,6 +26,11 @@ public class Background {
 		backgrounds.put("Snowy Mountain", 500);
 		backgrounds.put("Lava Cave", 1000);
 	}
+	
+	/** 
+	 * Buying a background with points. If not enough points or if the shop doesn't have the background, nothing will happen. 
+	 * @param backgroundchoice the background that the user is wishing to buy
+	 */
 	public void buyBackground(String backgroundchoice) {
 		if (backgrounds.containsKey(backgroundchoice)) {
 			int price = backgrounds.get(backgroundchoice);
@@ -36,17 +46,31 @@ public class Background {
 			System.out.println("Pick a map from the list");
 		}
 	}
+	/**
+	 * Checks the shop of backgrounds with their prices
+	 */
 	public void checkShop() {
 		System.out.println(backgrounds);
 	}
 
 
+	/**
+	 * changes to a new background that is owned by the user
+	 * @param backgroundChoice the new background the user wants to change to
+	 */
 	public void changeBackground(String backgroundChoice) {
 		if (owned.contains(backgroundChoice)) {
 			this.currentBackground= backgroundChoice;
 			System.out.println("Map changed!");
+		} else {
+			System.out.println("You don't own this background");
 		}
 	}
+	
+	/**
+	 * get the currently equipped background
+	 * @return the current background
+	 */
 	public String getBackground() {
 		return this.currentBackground;
 	}
