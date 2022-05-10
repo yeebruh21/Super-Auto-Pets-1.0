69b40ff78ba139d26eeb9da39209a3540cba564a
zshuhanzz <99359165+zshuhanzz@users.noreply.github.com>
2022-05-10 12:51:43 -0400

    Update Animal.java
    Modified Animal class for Main class

--- Animal.java
+++ Animal.java
@@ -9,32 +9,27 @@
 * - allow animal eat food 
 */
 
-import java.io.File;
-import java. util. Arrays;
-import java.io.FileReader;
-import java.io.BufferedReader; 
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 public class Animal {
-   /** Animal name */
-   private String name;
-   
-   /** animal health  */
-   private int health;
-   
-   /** number of directAttack */
-   private int directAttack;
-   
-   /** the level of each animal */
-   private int level;
-   
-   /** the tier of each animal */
-   private int tier;
-   
-   /** the ability of each animal */
-   private String ability;
-   
-   /**
+	/** Animal name */
+	private String name;
+
+	/** animal health  */
+	private int hp;
+
+	/** number of directAttack */
+	private int damage;
+
+	/** the level of each animal */
+	private int level;
+
+	/** the tier of each animal */
+	private int tier;
+
+
+	/**
    Create an animal 
    @param name
    @param health
@@ -42,163 +37,182 @@ public class Animal {
    @param level
    @param tier
    @param ability 
-   */
-   public Animal(String name, int health, int directAttack, int level, int tier,String ability) {
-      this.name = name;
-      this.health = health;
-      this.directAttack = directAttack;
-      this.level = level;
-      this.tier = tier;
-      this.ability= ability;
-      
-      // Restricting 
-      if (tier < 0) {
-         this.level = 1;
-      } else {
-         this.level = level;
-      }
-   }
-   
-   /*
+	 */
+	public Animal () {
+		
+	}
+	public Animal(String name, int damage, int hp, int level, int tier) {
+		this.name = name;
+		this.hp = hp;
+		this.damage = damage;
+		this.level = level;
+		this.tier = tier;
+
+		// Restricting 
+		if (tier < 0) {
+			this.level = 1;
+		} else {
+			this.level = level;
+		}
+	}
+
+	/*
    Accessors
-   */
-      
-   /**
+	 */
+
+	/**
    gets the Animal name
    @return the name of the Animal
-   */
-   public String getName() {
-      return this.name;
-   }
-      
-   /**
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
    gets the animal level one health
    @return the health of animal as level one
-   */
-   public int getHealth() {
-      return this.health;
-   }
-      
-   /**
+	 */
+	public int getHp() {
+		return this.hp;
+	}
+
+	/**
    gets the directAttack of the animal as level one
    @return the direct attack
-   */
-   public int getDirectAttack () {
-      return this.directAttack;
-   }
-      
-   /**
+	 */
+	public int getDmg () {
+		return this.damage;
+	}
+
+	/**
    gets the level of animal 
    @return the animal level
-   */
-   public int getLevel() {
-      return this.level;
-   }
-      
-   /**
+	 */
+	public int getLevel() {
+		return this.level;
+	}
+
+	/**
    gets the tier of animal 
    @return the tier of animal
-   */
-   public int getTier() {
-      return this.tier;
-   }
-      
-   /**
-   gets the ability of animal 
-   @return the ability in code form
-   */
-   public String getAbility() {
-      return this.ability;
-   }
-   
-   
-   
-   /**
+	 */
+	public int getTier() {
+		return this.tier;
+	}
+
+
+
+	public void addHp(int addedHp) {
+		this.hp+=addedHp;
+	}
+	public void addDmg(int addedDmg) {
+		this.damage+=addedDmg;
+	}
+
+	public String toString() {
+		return this.name+ " dmg: "+this.damage+" hp: "+this.hp;
+	}
+
+
+	/**
    read the information of a given animal in an array which includes the attirbutes in the following order 
    name, health, directAttack,level,tier, ability
-   */
-   
-   public void  generateAnimal (File address,String animalName) throws IOException
-   {
-   
-     //Create a new filereader which allows the main to call it using the file address containing the animal informations 
-     FileReader fr= new FileReader(address);
-     //
-     BufferedReader br= new BufferedReader(fr);
-     //
-     String line;
-      
-     while ((line= br.readLine())!=null){
-      
-         //first loop, check for all the words inside the sentence
-         for (int i = 0; i < 81 ; i++) {
-             int currIndex = 1; //used to count which letter we're at
-             String [] words = line.split(","); //spliting the sentence into words and saving it inside an array
-             
-             
-             //change the attirbutes of the animal asked for by reading the textfile that contains the info of each animal and finding the line that contains it
-             if (words [0]==animalName){
-             
-               this.name= words[0];
-               //casting the String version of attirbutes saved on the text file into int so it can be manipulated by other classes and methods
-               this.health=Integer.parseInt(words[1]);
-               this.directAttack=Integer.parseInt(words[2]);
-               this.level=Integer.parseInt(words[3]);
-               this.tier=Integer.parseInt(words[4]);
-               
-               this.ability=words[5];
+	 */
 
-             } 
-         }
-      
-     }
-     //closing the FileReader and bufferedReader
-     br.close();
-     fr.close(); 
-     
-    }
-    
-    /**
+	public static Animal generateAnimal (File address,String animalName) throws IOException
+	{
+
+		//Create a new filereader which allows the main to call it using the file address containing the animal informations 
+		FileReader fr= new FileReader(address);
+		//
+		BufferedReader br= new BufferedReader(fr);
+		//
+		String line;
+		
+		String name=null;
+		int hp=0;
+		int damage=0;
+		int level=0;
+		int tier=0;
+
+		while ((line= br.readLine())!=null){
+
+			//first loop, check for all the words inside the sentence
+			for (int i = 0; i < 10 ; i++) {
+				int currIndex = 1; //used to count which letter we're at
+				String [] words = line.split(","); //spliting the sentence into words and saving it inside an array
+
+				//change the attirbutes of the animal asked for by reading the textfile that contains the info of each animal and finding the line that contains it
+				if (words[0].equals(animalName)){
+
+					name= words[0];
+					//casting the String version of attirbutes saved on the text file into int so it can be manipulated by other classes and methods
+					hp=Integer.parseInt(words[1]);
+					damage=Integer.parseInt(words[2]);
+					level=Integer.parseInt(words[3]);
+					tier=Integer.parseInt(words[4]);
+					break;
+				} 
+			}
+
+		}
+		//closing the FileReader and bufferedReader
+		Animal a = new Animal(name, damage, hp, level, tier);
+		br.close();
+		fr.close();
+		return a; 
+
+	}
+
+	/**
     pick  a random level 1 animal from the list of 63 animals in standard package, instead of taking the name of animal from other classes
     @return a random animal from the list that includes all the animal info
-    */
-    
-   public  void getRandomAnimal (File address)throws IOException
-   
-   {
-     int animalNum = randomNumberGenerator(1, 63);//random number between 1 and 63 (number of animals on the animalinfo.txt ) 
-     //Create a new filereader which allows the main to call it using the file address containing the animal informations 
-     FileReader fr= new FileReader(address);
-     //
-     BufferedReader br= new BufferedReader(fr);
-     //
-     String line;      
-         //first loop, check for all the words inside the sentence
-         for (int i = 0; i <animalNum ; i++) {
-               line= br.readLine();
-               String [] words = line.split(","); //spliting the sentence into words and saving it inside an array
-               //change the attirbutes of the animal asked for by reading the textfile that contains the info of each animal and finding the line that contains it             
-               this.name= words[0];
-               //casting the String version of attirbutes saved on the text file into int so it can be manipulated by other classes and methods
-               this.health=Integer.parseInt(words[1]);
-               this.directAttack=Integer.parseInt(words[2]);
-               this.level=Integer.parseInt(words[3]);
-               this.tier=Integer.parseInt(words[4]);
-               this.ability=words[5];
-             } 
-             //closing the FileReader and bufferedReader
-             br.close();
-             fr.close(); 
-    }
-   
-   /**
+	 */
+
+	public static Animal getRandomAnimal (File address)throws IOException
+
+	{
+		int animalNum = randomNumberGenerator(1, 10);//random number between 1 and 63 (number of animals on the animalinfo.txt ) 
+		//Create a new filereader which allows the main to call it using the file address containing the animal informations 
+		FileReader fr= new FileReader(address);
+		//
+		BufferedReader br= new BufferedReader(fr);
+		//
+		String line;  
+		String name=null;
+		int hp=0;
+		int damage=0;
+		int level=0;
+		int tier=0;
+		//first loop, check for all the words inside the sentence
+		for (int i = 0; i <animalNum ; i++) {
+			line= br.readLine();
+			String [] words = line.split(","); //spliting the sentence into words and saving it inside an array
+			//change the attirbutes of the animal asked for by reading the textfile that contains the info of each animal and finding the line that contains it             
+			name= words[0];
+			//casting the String version of attirbutes saved on the text file into int so it can be manipulated by other classes and methods
+			hp=Integer.parseInt(words[1]);
+			damage=Integer.parseInt(words[2]);
+			level=Integer.parseInt(words[3]);
+			tier=Integer.parseInt(words[4]);
+			
+		} 
+		Animal a = new Animal(name, damage, hp, level, tier);
+		//closing the FileReader and bufferedReader
+		br.close();
+		fr.close();
+		return a; 
+	}
+
+	/**
    allows the geneerateRandomAnimal method to choose a random animal based on the # of lines in the database-textFile
    @returns a randomNum between the indicated range 
-   */
-   public static int randomNumberGenerator(int min, int max)
+	 */
+	public static int randomNumberGenerator(int min, int max)
 	{
 		double r = Math.random();
 		int randomNum = (int)(r * (max - min)) + min;//generate a random # using the random method of math class between the range of min and max
 		return randomNum;
 	}
 }
+
